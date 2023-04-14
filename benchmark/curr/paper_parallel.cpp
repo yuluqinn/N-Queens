1a33b31183cfa0597bb4d231fa2fe77084cec0de
Yulu <649783664@qq.com>
2023-04-14 17:55:28 -0400

    add the parallel version in the paper

---
+++ benchmark/curr/paper_parallel.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <omp.h>
+#include <chrono>
+
+using namespace std;
+
+int solutions = 0;
+int n;
+
+void setQueen(int positions[], int row, int col) {
+    for(int i=0; i<row; i++) {
+        // Check if position is safe
+        if (positions[i]==col || abs(positions[i]-col) == (row-i)) {
+            return;
+        }
+    }
+    // col is ok, set queen.
+    positions[row]=col;
+    if(row==n-1) {
+        #pragma omp atomic
+        solutions++;
+    } else {
+        for(int i=0; i<n; i++) {
+            setQueen(positions, row+1, i);
+        }
+    }
+}
+
+int main() {
+    cout << "Enter the size of the chessboard: ";
+    cin >> n;
+
+    int positions[n];
+
+    // Start timer
+    auto start_time = chrono::high_resolution_clock::now();
+
+    for(int i=0; i<n; i++) {
+        #pragma omp parallel for
+        setQueen(positions, 0, i);
+    }
+
+    // End timer
+    auto end_time = chrono::high_resolution_clock::now();
+
+    // Calculate duration
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
+
+    cout << "Number of solutions: " << solutions << endl;
+    cout << "Time taken: " << duration << " milliseconds" << endl;
+
+    return 0;
+}
