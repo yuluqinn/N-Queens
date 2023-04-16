868f0b8652b8216b9281b321e9b87c71efbd7423
Jiraphon Yenphraphai <jy3694@crunchy5.cims.nyu.edu>
2023-04-16 02:16:17 -0400

    f

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ g++ -fopenmp -o baseline baseline.cpp
 g++ -fopenmp -o nQueens nQueens.cpp
 g++ -fopenmp -o nQueens_task nQueens_task.cpp
 thread=4
-for i in {12..16}
+for i in {12..12}
 do
   echo "=====================baseline===$i====================================="
   time ./baseline -n $i
@@ -15,11 +15,11 @@ echo "*********************************************************************"
 for i in {12..17}
 do
   echo "=====================nQueens===$i====================================="
-  time ./nQueens -n $i -t thread
+  time ./nQueens -n $i -t $thread
 done
 
 for i in {12..17}
 do
   echo "=====================nQueens_task===$i====================================="
-  time ./nQueens_task -n $i -t thread -k 2
+  time ./nQueens_task -n $i -t $thread -k 2
 done
