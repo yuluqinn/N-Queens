73ca034b6704029f286087ac9b7b3524bed37e7d
domejiraphon <jyenphraphai@gmail.com>
2023-04-16 11:31:59 -0400

    cims

---
+++ run2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+g++ -fopenmp -o nQueens_task nQueens_task.cpp
+
+for t in 1 2 4 8 16
+do
+  echo "=====================nQueens_task===$i====================================="
+  time ./nQueens_task -n 17 -t $t -k 2
+done
